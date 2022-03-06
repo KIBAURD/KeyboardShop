560b2e6f3d0669a80fdf5bfab0295fd20e4ab57c
Marie Sommier <marie.a.s45@gmail.com>
2022-03-06 17:04:56 +0100

    added header and footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CardProductComponent } from './card-product/card-product.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CardProductComponent
+    CardProductComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
