f9d9d3b1bbbd42e04de7420a7292de0cdc4612ab
Marie Sommier <marie.a.s45@gmail.com>
2022-03-06 17:41:43 +0100

    date in french

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgModule,LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFR from '@angular/common/locales/fr';
+registerLocaleData(localeFR)
 import { AppComponent } from './app.component';
 import { CardProductComponent } from './card-product/card-product.component';
 import { HeaderComponent } from './header/header.component';
@@ -16,7 +18,10 @@ import { FooterComponent } from './footer/footer.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [{
+      provide : LOCALE_ID,
+      useValue :'fr-FR'
+    }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
